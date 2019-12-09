e94abd8b65db98f46d8f8736cca6473f03ca0baf
tss <tss@debian>
2019-12-09 09:25:14 +0100

    Esame Gestione Scheda

--- src/main/java/com/mycompany/esame/GestioneScheda.java
+++ src/main/java/com/mycompany/esame/GestioneScheda.java
@@ -14,52 +14,59 @@ import javax.swing.JOptionPane;
  */
 public class GestioneScheda {
 
+    static String nome;
+    static String cognome;
+    static int annoNascita;
+    static int anni;
+    static int annoCorrente = LocalDate.now().getYear();
+    static int fasciaGiovaneMin = 0;
+    static int fasciaGiovaneMax = 18;
+    static int fasciaAdultoMin = 18;
+    static int fasciaAdultoMax = 60;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-
-//impostare variabili per creare scheda
-        String nome;
-        String cognome;
-        int annoNascita;
-        int anni;
-        int annoCorrente = 2019;
-        int fasciaGiovaneMin = 0;
-        int fasciaGiovaneMax = 18;
-        int fasciaAdultoMin = 18;
-        int fasciaAdultoMax = 60;
-        String risultato = " ";
-
+        
         //chiedere all'utente i suoi dati
         nome = JOptionPane.showInputDialog("dimmi il nome");
         cognome = JOptionPane.showInputDialog("dimmi il cognome");
         String Nascita = JOptionPane.showInputDialog("dimmi la data di nascita");
         annoNascita = Integer.parseInt(Nascita);
-        
+
         //ottenere gli anni dell'utente dal suo anno di nascita
         anni = annoCorrente - annoNascita;
-        risultato += nome + " " + cognome + " nato/a nel " + annoNascita + " di anni " + anni + " " + "\n";
 
-        //creare condizioni secondo le quali in base agli anni  
-        //si ottiene la tipologia di utente
-        if (anni >= fasciaGiovaneMin && anni <= fasciaGiovaneMax) {
-            risultato += "tipo:" + "giovane" + "\n";
-        } else {
-        }
-
-        if (anni > fasciaAdultoMin && anni <= fasciaAdultoMax) {
-            risultato += "tipo:" + "adulto" + "\n";
-
-        } else {
-            risultato += "tipo:" + "senior" + "\n";
-
-        }
+        
+        String scheda = stampascheda();
 
         //stampare l'output
-        System.out.println(risultato);
+        System.out.println(stampascheda() + Tipologia());
+    }
 
+    //metodo per stampare scheda con informazioni ottenute dall'input
+    static String stampascheda() {
+        String risultato = " ";
+        risultato += " nome: " + nome + " cognome: " + cognome + " nato/a nel " + annoNascita + " di anni " + anni + " " + "\n";
+        return risultato;
+    }
+    
+    //metodo con if per torvare la tipologia giovane,adulto,senior
+    static String Tipologia () {
+        String ris = " ";
+        
+        if (anni >= fasciaGiovaneMin && anni <= fasciaGiovaneMax) {
+            ris += "tipo:" + "giovane" + "\n";
+        } else {
+            if (anni > fasciaAdultoMin && anni <= fasciaAdultoMax) {
+                ris += "tipo:" + "adulto" + "\n";
+            } else {
+                ris += "tipo:" + "senior" + "\n";
+            }
+        }
+        return ris;
     }
 
 }
