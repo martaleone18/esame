3c30a8a6e743006c20736379d23bffa672eed4db
tss <tss@debian>
2019-12-09 10:46:40 +0100

    Esame Gestione Scheda personale

--- src/main/java/com/mycompany/esame/GestioneScheda.java
+++ src/main/java/com/mycompany/esame/GestioneScheda.java
@@ -29,7 +29,7 @@ public class GestioneScheda {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         //chiedere all'utente i suoi dati
         nome = JOptionPane.showInputDialog("dimmi il nome");
         cognome = JOptionPane.showInputDialog("dimmi il cognome");
@@ -38,25 +38,21 @@ public class GestioneScheda {
 
         //ottenere gli anni dell'utente dal suo anno di nascita
         anni = annoCorrente - annoNascita;
-
-        
         String scheda = stampascheda();
 
         //stampare l'output
         System.out.println(stampascheda() + Tipologia());
     }
-
     //metodo per stampare scheda con informazioni ottenute dall'input
     static String stampascheda() {
         String risultato = " ";
         risultato += " nome: " + nome + " cognome: " + cognome + " nato/a nel " + annoNascita + " di anni " + anni + " " + "\n";
         return risultato;
     }
-    
     //metodo con if per torvare la tipologia giovane,adulto,senior
-    static String Tipologia () {
+    static String Tipologia() {
         String ris = " ";
-        
+
         if (anni >= fasciaGiovaneMin && anni <= fasciaGiovaneMax) {
             ris += "tipo:" + "giovane" + "\n";
         } else {
@@ -68,5 +64,4 @@ public class GestioneScheda {
         }
         return ris;
     }
-
 }
